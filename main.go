3289172aaffde9c8730998c639039a618d7c1a76
Eduard Mocanu <mocanueduardgabi@gmail.com>
2026-09-17 13:20:19 +0300

    added the limiter struct

--- main.go
+++ main.go
@@ -29,17 +29,34 @@ func (b *Bucket) Allow(now time.Time) bool {
 	return false
 }
 
-func main() {
+type Limiter struct {
+	users      map[string]*Bucket
+	capacity   float64
+	refillRate float64
+}
 
-	b := Bucket{
-		tokens:     3,
-		lastRefill: time.Now(),
-		capacity:   20,
-		refillRate: 0.1,
+func (limiter *Limiter) Allow(key string, now time.Time) bool {
+	value, ok := limiter.users[key]
+	if !ok {
+		value = &Bucket{
+			tokens:     limiter.capacity,
+			lastRefill: now,
+			capacity:   limiter.capacity,
+			refillRate: limiter.refillRate,
+		}
+		limiter.users[key] = value
 	}
+	return value.Allow(now)
+}
 
-	for range 15 {
-		time.Sleep(time.Second * 1)
-		fmt.Println(b.Allow(time.Now()))
+func NewLimiter(capacity, refillRate float64) *Limiter {
+	return &Limiter{users: make(map[string]*Bucket), capacity: capacity, refillRate: refillRate}
+}
+
+func main() {
+	limiter := NewLimiter(3, 0.2)
+	now := time.Now()
+	for range 5 {
+		fmt.Println("alice:", limiter.Allow("alice", now), " bob:", limiter.Allow("bob", now))
 	}
 }
